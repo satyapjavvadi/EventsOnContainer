5aa3a389e8e8e7f4ec1e4b9325f8961e783fb8f1
CarleeSinton <carleen.sinton@gmail.com>
2019-09-18 12:30:32 -0700

    Database Table Formatting
    Adding configuration settings for when database tables are created when an event is added.

--- EventCatalogAPI/Data/CatalogContext.cs
+++ EventCatalogAPI/Data/CatalogContext.cs
@@ -26,19 +26,43 @@ namespace EventCatalogAPI.Data
             eventModelBuilder.Entity<CatalogItem>(ConfigureCatalogItem);
         }
 
-        private void ConfigureCatalogCategory(EntityTypeBuilder<CatalogCategory> obj)
+        private void ConfigureCatalogCategory(EntityTypeBuilder<CatalogCategory> categoryBuilder)
         {
-            throw new NotImplementedException();
+            categoryBuilder.ToTable("CatalogCategories");
+            categoryBuilder.Property(c => c.ID).IsRequired().ForSqlServerUseSequenceHiLo("catalog_category_hilo");
+            categoryBuilder.Property(c => c.Category).IsRequired().HasMaxLength(100);
+            
         }
 
-        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> obj)
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> typeBuilder)
         {
-            throw new NotImplementedException();
+            typeBuilder.ToTable("CatalogTypes");
+            typeBuilder.Property(t => t.ID).IsRequired().ForSqlServerUseSequenceHiLo("catalog_type_hilo");
+            typeBuilder.Property(t => t.Type).IsRequired().HasMaxLength(100);
         }
 
-        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> obj)
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> itemBuilder)
         {
-            throw new NotImplementedException();
+            itemBuilder.ToTable("Events");
+            itemBuilder.Property(e => e.Id).IsRequired().ForSqlServerUseSequenceHiLo("events_hilo");
+            itemBuilder.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            itemBuilder.Property(e => e.Information).IsRequired();
+            itemBuilder.Property(e => e.AddressLineOne).IsRequired();
+            itemBuilder.Property(e => e.AddressLineTwo).HasMaxLength(100);
+            itemBuilder.Property(e => e.City).IsRequired().HasMaxLength(100);
+            itemBuilder.Property(e => e.State).IsRequired().HasMaxLength(50);
+            itemBuilder.Property(e => e.Country).IsRequired().HasMaxLength(100);
+            itemBuilder.Property(e => e.Zipcode).IsRequired();
+            itemBuilder.Property(e => e.Time).IsRequired();
+            itemBuilder.Property(e => e.PictureURL).IsRequired();
+            //Are these necessary? Each is not required
+            itemBuilder.Property(e => e.Price);
+            itemBuilder.Property(e => e.NumberOfTickets);
+            itemBuilder.Property(e => e.PresenterName);
+
+
+            itemBuilder.HasOne(e => e.CatalogCategory).WithMany().HasForeignKey(e => e.CatalogCategoryID);
+            itemBuilder.HasOne(e => e.CatalogType).WithMany().HasForeignKey(e => e.CatalogTypeID);
         }
     }
 }
