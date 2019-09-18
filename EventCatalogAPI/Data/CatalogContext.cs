b77ba57b9c9dc0f5590a8a3a30fddee69f7a52a2
CarleeSinton <carleen.sinton@gmail.com>
2019-09-18 12:24:05 -0700

    Add Catalog Categories, Types, and Items
    Set up definition for the event catalog's categories, types, and items.

---
+++ EventCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,44 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public DbSet<CatalogCategory> CatalogCategories { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder eventModelBuilder)
+        {
+            eventModelBuilder.Entity<CatalogCategory>(ConfigureCatalogCategory);
+            eventModelBuilder.Entity<CatalogType>(ConfigureCatalogType);
+            eventModelBuilder.Entity<CatalogItem>(ConfigureCatalogItem);
+        }
+
+        private void ConfigureCatalogCategory(EntityTypeBuilder<CatalogCategory> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EventCatalogAPI/Domain/CatalogCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class CatalogCategory
+    {
+        public int ID { get; set; }
+        public string Category { get; set; }
+    }
+}
