390879faf968f96957b47f86c14bdf00382af7ad
CarleeSinton <carleen.sinton@gmail.com>
2019-09-19 14:39:07 -0700

    CatalogController additions
    added ability to display event catalog items in pages with 6 items

--- EventCatalogAPI/Controllers/CatalogController.cs
+++ EventCatalogAPI/Controllers/CatalogController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using EventCatalogAPI.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EventCatalogAPI.Controllers
 {
@@ -11,8 +15,29 @@ namespace EventCatalogAPI.Controllers
     [ApiController]
     public class CatalogController : ControllerBase
     {
-        public IActionResult Items(int pageIndex = 0, int pageSize = 6)
+        private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items([FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 6)
+        {
+            var itemsCount = await _context.CatalogItems.LongCountAsync();
+            var items = await _context.CatalogItems.OrderBy(c => c.Name).Skip(pageIndex * pageSize).Take(pageSize).ToListAsync();
+            items = ChangePictureURL(items);
+            return Ok(items);
+
+        }
+
+        private List<CatalogItem> ChangePictureURL(List<CatalogItem> items)
+        {
+            items.ForEach(c => c.PictureURL = c.PictureURL.Replace("http://externalcatalogbaseurltobereplaced", _config["ExternalCatalogBaseURL"]));
+            return items;
 
         }
     }
